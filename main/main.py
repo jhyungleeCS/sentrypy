9da50c45d204bfa0e2629c367579f32e9e764bae
Daniel Lee <104159089+jhyungleeCS@users.noreply.github.com>
2024-04-23 22:06:01 -0400

    test

--- main/main.py
+++ main/main.py
@@ -9,10 +9,15 @@ import re
 def read_txtfile():
     filename = "../log files/" + input("filename: ") + '.txt'
     pattern = r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    addys = []
     with open(filename, 'r') as file: 
         for line in file: 
             ip_add = re.search(pattern,line)
-            print(ip_add.group())
+            #print(ip_add.group())
+            addys.append(ip_add.group())
+        print(addys)
+            
+        
 
 
 read_txtfile()
--- main/main.sh
+++ main/main.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 tcpdump -i eth0 -s 0 -A 'tcp dst port 80 or tcp dst port 443' -l > httptraffic.txt
+tcpdump -i eth0 -XA 
